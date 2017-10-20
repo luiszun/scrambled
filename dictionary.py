af33f5015949226a8a653d70f4df8304a34f30bf
Luis Zuniga <luisangel.cpp@gmail.com>
2017-10-19 17:54:25 -0700

    Simple implementation of a in-memory tree for the dictionary

---
+++ dictionary.py
@@ -0,0 +1,53 @@
+#!/usr/env/python
+
+class Node:
+	def __init__ (self):
+		self.isWord = False
+		self.children = {}
+
+class Dictionary:
+	def __init__ (self):
+		self.wordCount = 0
+		self.Tree = Node()
+		self.charCount = {}
+
+	def loadDictionary (self, filename):
+		file = open(filename, "r")
+		for i in file:
+			words = i.rstrip().split(",")
+			for word in words:
+				self.insertWord(word)
+
+	def insertWord (self, word):
+		inode = self.Tree
+		for i, char in enumerate(word):
+			char = char.lower()
+
+			if not char.isalpha():
+				return False
+
+			if char not in inode.children:
+				inode.children[char] = Node()
+			inode = inode.children[char]
+			if i == len(word)-1:
+				inode.isWord = True
+
+			if char in self.charCount:
+				self.charCount[char] += 1
+			else:
+				self.charCount[char] = 1
+
+	def searchWord (self, word):
+		inode = self.Tree
+		for i, char in enumerate(word):
+			char = char.lower()
+			if char not in inode.children:
+				return False
+			inode = inode.children[char]
+
+			if i == len(word)-1:
+				return inode.isWord
+		# Empty string
+		return False
+
+
